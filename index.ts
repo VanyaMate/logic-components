0a5b926080507096885cc284d6b071f61ca7f63f
VanyaMate <buhu653@mail.ru>
2023-10-29 05:40:22 +0300

    up

--- index.ts
+++ index.ts
@@ -10,9 +10,15 @@ const onCheckboxInit = function (state: boolean): void {
     console.log('Init', state);
 };
 
+const subscribeChangeHandler = function (state: boolean): void {
+    console.log('Subscribe', state);
+};
+
 const checkbox: ICheckbox = new Checkbox(false, {
     init  : [ onCheckboxInit ],
     change: [ onChangeHandler ],
 });
 
 checkbox.toggle().then((status) => console.log('PromiseChange', status));
+checkbox.subscribe('change', subscribeChangeHandler);
+checkbox.toggle();
